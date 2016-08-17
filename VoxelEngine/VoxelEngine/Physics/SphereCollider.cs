974f0633ccb02bd99c0a3eed4dd9c6df3704c4ef
michael <michael@felleisen.de>
2016-08-17 12:14:53 +0200

    laptop auf aktuellen stand bringen

---
+++ VoxelEngine/VoxelEngine/Physics/SphereCollider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace VoxelEngine.Physics
+{
+    class SphereCollider : Collider
+    {
+        public float Range;
+
+        public SphereCollider(Vector3 position, float range) : base(position, 0)
+        {
+            Range = range;
+        }
+
+    }
+}
