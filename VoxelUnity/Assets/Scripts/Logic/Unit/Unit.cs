751e835540e8edb9fc6241aa8c54dbd6f4ad36ca
Mikah <michael@felleisen.de>
2016-10-10 17:28:52 +0200

    unit + unitcreator most likely up and running (not tested)

---
+++ VoxelUnity/Assets/Scripts/Logic/Unit/Unit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Assets.Scripts.Logic.Unit
+{
+    public class Unit
+    {
+        public BaseValues BaseValues;
+        public int Race;
+
+        public Unit(BaseValues values, int race)
+        {
+            BaseValues = values;
+            Race = race;
+        }
+
+
+    }
+
+    public struct BaseValues
+    {
+        public float Hitpoints;
+        public float Condition;
+        public float Strength;
+        public float Dexterity;
+        public float Intelligence;
+        public float Wisdom;
+    }
+}
+
