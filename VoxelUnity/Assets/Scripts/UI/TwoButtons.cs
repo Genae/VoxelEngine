ddd1c9a6c5a9c190248bdb3f5c8f1d9597e818cf
MININT-2KVESV2\Flo <florian.hoeschl@googlemail.com>
2017-08-13 01:51:44 +0200

    merge

---
+++ VoxelUnity/Assets/Scripts/UI/TwoButtons.cs
@@ -0,0 +1,22 @@
+﻿using MarkLight.Views.UI;
+using UnityEngine;
+using Assets.Scripts.Logic;
+
+namespace Assets.Scripts.Control
+{
+	
+public class TwoButtons : UIView
+{
+	public void ButtonMine(){
+		//0 = mineTool atm
+		var go = GameObject.Find ("World").GetComponent<MouseController>();
+		go.SelectedTool = go.Tools [0];
+	}
+
+	public void ButtonBuild(){
+		var go = GameObject.Find ("World").GetComponent<MouseController>();
+		go.SelectedTool = go.Tools [1];
+	}
+}
+
+}
