22297a98eb642943bc064efe9fbfefdfbaeb52f4
Stefan Stark <stefan@2stark4you.de>
2017-08-15 19:40:44 +0200

    Add GameSpeed Buttons

---
+++ VoxelUnity/Assets/Scripts/UI/GameSpeed.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Logic;
+using MarkLight.Views.UI;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+	public class GameSpeed : UIView
+    {
+        public void ClickNormal()
+        {
+            Time.timeScale = 1;
+        }
+
+        public void ClickFast()
+	    {
+	        Time.timeScale = 2;
+	    }
+
+	    public void ClickVeryFast()
+        {
+	        Time.timeScale = 5;
+        }
+        
+        public void ClickSlow()
+        {
+            Time.timeScale = 0.3f;
+        }
+    }
+
+}
