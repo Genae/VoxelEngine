87b9a94e8f5ff6fcc2d62c49f0a78558d3973a66
Genae <stefan@2stark4you.de>
2017-11-14 19:11:07 +0100

    fix exploder

--- VoxelUnity/Assets/Scripts/EngineLayer/Voxels/Containers/Multiblock/Fraction.cs
+++ VoxelUnity/Assets/Scripts/EngineLayer/Voxels/Containers/Multiblock/Fraction.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 namespace Assets.Scripts.EngineLayer.Voxels.Containers.Multiblock
 {
     public class Fraction : MonoBehaviour {
+        private float _force;
+        private Vector3 _middle;
 
-        public void Init(Vector3 position, float scale, Color color, Vector3 middle, float force = 500f)
+        public void Init(Vector3 position, float scale, Color color, Vector3 middle, float force = 10f)
         {
+            _force = force;
+            _middle = middle;
             transform.position = position;
             transform.localScale = Vector3.one * scale;
             transform.GetComponent<MeshRenderer>().material.color = color;
-            transform.GetComponent<Rigidbody>().AddExplosionForce(force, middle, 20f);
+            Invoke("Explode", 0.1f);
             Invoke("Destroy", Random.Range(3f, 4f));
         }
+
+        public void Explode()
+        {
+            transform.GetComponent<Rigidbody>().AddExplosionForce(_force, _middle, 20f, 1f, ForceMode.VelocityChange);
+        }
+
 
         public void Destroy()
         {
