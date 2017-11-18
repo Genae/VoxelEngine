0d62301fc9a7755158c119693ed9eb17aa2ae35a
Mikah <michael@felleisen.de>
2017-11-18 12:56:30 +0100

    Start scene added, scene build order beginnings

---
+++ VoxelUnity/Assets/Scripts/GameLogicLayerTD/StartScreenLogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenLogic : MonoBehaviour
+{
+    private int buttonW = 300;
+    private int buttonH = 200;
+
+    //Logic lol
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width/2 - buttonW/2, Screen.height/2 - buttonH/2, buttonW, buttonH), "start campaign"))
+        {
+            SceneManager.LoadScene(1); // 1 is TDAR_Scene, 0 is StartScreen
+        }
+    }
+}
