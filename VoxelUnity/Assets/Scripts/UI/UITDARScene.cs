899aaade22b75c0043ed98490be65256386db0d9
Genae <stefan@2stark4you.de>
2017-11-20 21:52:43 +0100

    ar fixes

---
+++ VoxelUnity/Assets/Scripts/UI/UITDARScene.cs
@@ -0,0 +1,26 @@
+﻿using MarkLight;
+using Assets.Scripts.EngineLayer.Voxels.Containers;
+using UnityEngine;
+using Assets.Scripts.GameLogicLayerTD;
+
+namespace Assets.Scripts.UI
+{
+    public class UITDARScene : View
+    {
+        public void ClickStart()
+        {
+            var tdMap = Map.Instance.gameObject.AddComponent<TDMap>();
+            tdMap.BuildMap();
+        }
+
+
+        public void ClickClear()
+        {
+            Destroy(Map.Instance.gameObject.GetComponent<TDMap>());
+            foreach (Transform child in Map.Instance.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
