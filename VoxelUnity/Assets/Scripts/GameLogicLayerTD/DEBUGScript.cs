527d16adbd112d4ffb784dc26b5447b205bba7df
Mikah <michael@felleisen.de>
2017-11-20 22:15:36 +0100

    DEBUGScript added

---
+++ VoxelUnity/Assets/Scripts/GameLogicLayerTD/DEBUGScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUGScript : MonoBehaviour
+{
+    public int DEBUG_INDEX = 0;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 50 * DEBUG_INDEX, 200,200), transform.name + " : " + transform.position.ToString());
+    }
+}
