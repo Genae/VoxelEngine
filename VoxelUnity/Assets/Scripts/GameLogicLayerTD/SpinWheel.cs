45cae6b35d856d24b6f49623a520adbbb055b3f9
michael <michael@felleisen.de>
2018-01-24 17:10:16 +0100

    catapult_root and ram_root added, spin wheel script added :}

---
+++ VoxelUnity/Assets/Scripts/GameLogicLayerTD/SpinWheel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinWheel : MonoBehaviour {
+
+	
+	void Update () {
+        transform.RotateAroundLocal(Vector3.forward, -Time.deltaTime);
+	}
+}
