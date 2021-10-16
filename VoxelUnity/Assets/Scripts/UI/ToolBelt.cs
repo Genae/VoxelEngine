622c4bd76a3a74f07ccc7267fda3f044f439230c
Stefan Stark <stefan@2stark4you.de>
2021-10-16 21:00:06 +0200

    UI fixed

--- VoxelUnity/Assets/Scripts/UI/ToolBelt.cs
+++ VoxelUnity/Assets/Scripts/UI/ToolBelt.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Assets.Scripts.ControlInputs;
 using Assets.Scripts.EngineLayer;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
-	public class ToolBelt
+	public class ToolBelt: MonoBehaviour
 	{
 	    public List<ToolConfig> MainGroup;
 	    public ToolConfig SelectedItemMain;
-	    public List<ToolConfig> SecondaryGroup = new List<ToolConfig>();
 	    public ToolConfig SelectedItemSecondary;
 	    public Dictionary<string, ToolConfig[]> AllSecondaryGroups = new Dictionary<string, ToolConfig[]>();
 	    public int xOffset;
+	    
+	    public GameObject ButtonPrefab;
+	    public GameObject PanelPrefab;
+	    public GameObject MainPanel;
+	    public Dictionary<string,GameObject> Panels = new Dictionary<string, GameObject>();
 
 	    private MouseController _controller;
 
@@ -23,33 +30,79 @@ namespace Assets.Scripts.UI
             MainGroup = new List<ToolConfig>();
             foreach (var config in configs)
             {
+	            MainPanel.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 100*config.Length);
                 foreach (var toolConfig in config)
                 {
                     MainGroup.Add(toolConfig);
+                    var b = Instantiate(ButtonPrefab, MainPanel.transform);
+                    var tb = b.AddComponent<ToolButton>();
+                    tb.Init(toolConfig, this);
                     if (toolConfig.Children != null && toolConfig.Children.Length > 0)
                     {
+	                    Panels.Add(toolConfig.Name, Instantiate(PanelPrefab, transform));
+	                    Panels[toolConfig.Name].GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 100*toolConfig.Children.Length);
+	                    Panels[toolConfig.Name].SetActive(false);
                         AllSecondaryGroups[toolConfig.Name] = toolConfig.Children;
+                        foreach (var toolConfigChild in toolConfig.Children)
+                        {
+	                        b = Instantiate(ButtonPrefab, Panels[toolConfig.Name].transform);
+	                        tb = b.AddComponent<ToolButton>();
+	                        tb.Init(toolConfigChild, this);
+                        }
                     }
                 }
                 
             }
         }
 
-	    public void SelectedMain()
+	    public void SelectedMain(ToolConfig main)
 	    {
-	        var list = AllSecondaryGroups[SelectedItemMain.Name];
-
+		    SelectedItemMain = main;
+	        
 	        xOffset = (int) ((MainGroup.IndexOf(SelectedItemMain) + 0.5f - MainGroup.Count / 2f) * 100);
+	        foreach (var panel in Panels)
+	        {
+		        panel.Value.gameObject.SetActive(false);
+		    }
+	        Panels[SelectedItemMain.Name].SetActive(true);
 
-	        SecondaryGroup.Clear();
-            SecondaryGroup.AddRange(list);
-            //_controller.SelectedTool = null;
+            _controller.SelectedTool = null;
 
 	    }
-	    public void SelectedSecondary()
+	    public void SelectedSecondary(ToolConfig sec)
 	    {
+		    SelectedItemSecondary = sec;
 	        _controller.SelectTool(SelectedItemSecondary.Tool);
         }
     }
+
+	public class ToolButton: MonoBehaviour
+	{
+		private ToolConfig _config;
+		private ToolBelt _parent;
+
+		public void Init(ToolConfig config, ToolBelt parent)
+		{
+			_config = config;
+			_parent = parent;
+			transform.GetComponentInChildren<Text>().text = _config.Name;
+			if(config.Children?.Any()??false)
+				GetComponent<Button>().onClick.AddListener(MainSelected);
+			else
+			{
+				GetComponent<Button>().onClick.AddListener(SecondarySelected);
+			}
+		}
+
+		private void SecondarySelected()
+		{
+			_parent.SelectedSecondary(_config);
+		}
+
+		private void MainSelected()
+		{
+			_parent.SelectedMain(_config);
+		}
+	}
 
 }
